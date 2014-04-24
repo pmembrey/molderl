15274beeea6792233b8e68cdb665b1f079af7498
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2014-04-24 14:16:18 +0800

    removed API call added due to confusion

--- src/molderl_stream.erl
+++ src/molderl_stream.erl
@@ -3,7 +3,7 @@
 
 -behaviour(gen_server).
 
--export([start_link/7, send/2, send/3]).
+-export([start_link/7, send/3]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
@@ -35,9 +35,6 @@ start_link(SupervisorPid, StreamName, Destination, DestinationPort,
                           [SupervisorPid, StreamName, Destination, DestinationPort,
                            RecoveryPort, IPAddressToSendFrom, Timer],
                           []).
-
-send(Pid, Message) ->
-    gen_server:cast(Pid, {send, Message, os:timestamp()}).
 
 send(Pid, Message, StartTime) ->
     gen_server:cast(Pid, {send, Message, StartTime}).
